776f7f1cf438f4e06675c9e73fbee80e5ad97926
Ersin Evci <r0837670@ucll.be>
2025-10-07 15:06:51 +0200

    05-strings solved

--- 05-strings/recap-exercise-strings-encryption-decryption/student.py
+++ 05-strings/recap-exercise-strings-encryption-decryption/student.py
@@ -1,1 +1,37 @@
 # write your code here
+def decode1(text):
+    return text.replace("A","o")
+
+
+# print(decode1('SchAAl'))
+
+
+def decode2(text):
+   new_text = text[0::2]
+   return new_text
+
+# print(decode2('hqovtzdpozgm'))
+
+
+def decode3(text):
+    new_text =  text.split(" ")
+    # print(new_text)
+    new_text[0] = new_text[0][::-1] 
+    joined_text = " ".join(new_text)
+    return joined_text
+
+
+# decode3("sepocseleT are too expensive.")    
+
+def decode4(text):
+    length = 0
+    
+    if len(text)%2 ==0:
+        length= len(text)//2+2
+    else:
+        length = len(text)//2+3
+        
+
+    return text[2:length]
+
+# print(decode4('oddolfijnnjifls'))
